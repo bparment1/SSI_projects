34570bc23d3fbd84e248877f84ab0ae3c076898c
Benoit Parmentier <benoit.parmentier@gmail.com>
2014-05-09 10:45:00 -0400

    adding column region ID input shapefile and reorganization of code

--- DSS_SSI_workflow_database_prep.py
+++ DSS_SSI_workflow_database_prep.py
@@ -30,7 +30,7 @@ from osgeo import osr        #Georeferencing: Spatial refreferen system for geog
 from osgeo import gdal_array #gdal geographic library
 from osgeo import gdalconst  #gdal geographic library
 import psycopg2              # Postgres binding, SQL query etc
-import numpy                 #Array, matrices and scientific computing
+import numpy as np           #Array, matrices and scientific computing
 import pickle                #Object serialization
 from multiprocessing import Process, Manager, Pool #parallel processing
 import pdb                   #for debugging
@@ -41,6 +41,7 @@ import pandas as pd
 # Functions used in the script 
 #------------------
 
+    
 def create_dir_and_check_existence(path):
     #Create a new directory
     try:
@@ -126,27 +127,175 @@ def shp_to_data_frame(shp_fname):
         print "Could not open file"
         sys.exit(1)
     lyr= ds.GetLayer(0)    
-    feature= lyr.GetFeature(0)
+    #feature= lyr.GetFeature(0)
     #Now get the attributes...
     #make this a function
     layerDefinition = lyr.GetLayerDefn()
     column_names = []
-    ncolumn = layerDefinition.GetFieldCount()
+    #ncolumn = layerDefinition.GetFieldCount()
     for i in range(layerDefinition.GetFieldCount()):       
         column_names.append(layerDefinition.GetFieldDefn(i).GetName())
     #table = {}
-    value_att = []
-    list_values = [] 
+    
+    #list_values = []
+    dict_values = {} #this will store the column values...
     for column in column_names:
         name=column
-        for feature in lyr:               
-            value=feature.GetField(name)
-            value_att.append(value)
+        value_att = []
+        #value_att= {}
+        for i in range(lyr.GetFeatureCount()):
+            #f is a featuur 
+            f =lyr.GetFeature(i)
+            val = f.GetField(name)
+            value_att.append(val)
+            f.Destroy()
             
-        list_values.append(value_att)
+            #value_att.append(f.GetField(name))
+            #value_att.update(f.GetField(name))
+            #f.Destroy()
+        #Maybe should make val_att a data.frame here or a series... 
+        #pd.Series()
+        d = {name:value_att}
+        #list_values.append(value_att)
+        dict_values.update(d)
+    #now make it a data.frame
+    df_table = pd.DataFrame(dict_values,columns=column_names)
+    #df_table = pd.DataFrame(list_values,columns=column_names)
+    return df_table
 
     #print "Name  -  Type  Width  Precision"
     
+
+def add_field_to_shp(in_fname,out_fname,dict_col=None):
+    #This functions add a field to a shape file. It can also be used to make a copy of the shapefil.
+    #Add option for reprojection!!!
+    #Also clean so it works for any vector type (ie other than ESRI Shapefile)
+    
+    ### START SCRIPT ####
+    
+    OGRTypes = {int: ogr.OFTInteger, str: ogr.OFTString, float: ogr.OFTReal} #note there are missing types!!
+
+    #Set driver
+    DriverName = "ESRI Shapefile"      # e.g.: GeoJSON, ESRI Shapefile
+     
+    #Read in data source 
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    ds =driver.Open(in_fname,1) #writeable for one
+    if ds is None:
+        print "Could not open file"
+        sys.exit(1)
+        
+    #Get the input layer    
+    lyr = ds.GetLayer(0)    
+     
+    #Create destination data file and source 
+    driver = ogr.GetDriverByName(DriverName)
+    if os.path.exists(out_fname):
+        driver.DeleteDataSource(out_fname)
+
+    dst_ds = driver.CreateDataSource( out_fname )
+    
+    #Create output laer
+    
+    #proj = osr.SpatialReference()  
+    #proj.SetWellKnownGeogCS( "EPSG:4326" )  
+    proj =lyr.GetSpatialRef()
+    geometry_type = lyr.GetLayerDefn().GetGeomType()    
+    #outLayer = outDataSource.CreateLayer("states_convexhull", geom_type=geometry_type)
+    dst_layer = dst_ds.CreateLayer('region',
+                            srs = proj,
+                            geom_type=geometry_type)
+    inFeature  = lyr.GetFeature(0)
+    
+    
+    # add fields: use input layer definitions
+    inLayerDefn = lyr.GetLayerDefn()
+    for i in range(0, inLayerDefn.GetFieldCount()):
+        fieldDefn = inLayerDefn.GetFieldDefn(i)
+        dst_layer.CreateField(fieldDefn)
+
+    # get the output layer's feature definition
+    dst_layer_Defn = dst_layer.GetLayerDefn()
+
+    # loop through the input features
+    #inFeature = inLayer.GetNextFeature()
+
+    #Now copy the feature in the new layer
+    #reproject if True 
+
+    for j in range(lyr.GetFeatureCount()):
+        #f is a featuur 
+        # get the input geometry
+        inFeature = lyr.GetFeature(j) #getting feature i    
+        geom = inFeature.GetGeometryRef() #get geometry
+        # reproject the geometry
+        #geom.Transform(coordTrans)
+        # create a lynew feature
+        feature_new = ogr.Feature(dst_layer_Defn)
+        # set the geometry and attribute
+        feature_new.SetGeometry(geom)
+        for i in range(0, dst_layer_Defn.GetFieldCount()):
+            feature_new.SetField(dst_layer_Defn.GetFieldDefn(i).GetNameRef(), inFeature.GetField(i))
+
+            
+        # add the feature to the shapefile
+        dst_layer.CreateFeature(feature_new)
+        # destroy the features and get the next input feature
+        feature_new.Destroy()
+        inFeature.Destroy()
+
+    #CLOSE SHAPEFILES...
+    dst_ds.Destroy()
+    ds.Destroy()
+
+
+    #If new field is True:
+    if dict_col is None:
+        print "No field added "
+    #    dst_ds.Destroy()
+    #    ds.Destroy()
+        sys.exit(1)
+
+    #Read in data source 
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    dst_ds =driver.Open(out_fname,1) #writeable for one
+    if ds is None:
+        print "Could not open file"
+        sys.exit(1)
+    dst_layer = dst_ds.GetLayer(0)    
+    # get the output layer's feature definition
+    dst_layer_Defn = dst_layer.GetLayerDefn()
+
+    field_name = dict_col.keys()[0]
+    data_type =  type(dict_col.values()[0][0]) #get python data type from the first item in the dict 
+    new_field =  ogr.FieldDefn(field_name, OGRTypes[data_type])
+    #fldDef = ogr.FieldDefn('Name', ogr.OFTString)
+    #fldDef2.SetWidth(16) #16 char string width
+    dst_layer.CreateField(new_field)
+    
+    #inFeature = inLayer.GetNextFeature()
+    #while inFeature:
+
+    for j in range(dst_layer.GetFeatureCount()):
+        #f is a featuur 
+            # get the input geometry
+        inFeature = dst_layer.GetFeature(j) #getting feature i    
+        val = dict_col.values()[0][j]
+
+        inFeature.SetField(field_name,val) # assign value to feature for new field
+        #dst_layer.SetFeature(feat) 
+        # add the feature to the shapefile
+        #dst_layer.CreateFeature(inFeature)
+        dst_layer.SetFeature(inFeature)  #NEED TO USE SET FEATURE NOT CREATE FEATURE!!!
+        
+        # destroy the features and get the next input feature
+        #feature_new.Destroy()
+        inFeature.Destroy()
+
+    #CLOSE SHAPEFILES...
+    dst_ds.Destroy()
+    #ds.Destroy()
+    #END OF FUNCTION
 
 def get_vct_FieldName(shp_fname):
     driver = ogr.GetDriverByName("ESRI Shapefile")
@@ -168,9 +317,11 @@ def get_vct_FieldName(shp_fname):
         info = [fieldName,fieldType,str(fieldWidth),GetPrecision]
         list_names.append(info)
         #print fieldName + " - " + fieldType+ " " + str(fieldWidth) + " " + str(GetPrecision)
-    return list_names
+    nf = lyr.GetFeatureCount() #number of features
+    return list_names, nf
 
-def caculate_zonal_statistics(i,out_dir,out_suffix,rast_fname,shp_fname,SRS_EPSG,postgres_path_bin,db_name,user_name):
+def caculate_zonal_statistics(i,out_dir,out_suffix,rast_fname,shp_fname,SRS_EPSG,region_id_col,postgres_path_bin,db_name,user_name):
+#def caculate_zonal_statistics(i,out_dir,out_suffix,rast_fname,shp_fname,SRS_EPSG,postgres_path_bin,db_name,user_name):
     
     #This function calculate summary statistic i.e. average per region in the
     #input shapefile.
@@ -290,11 +441,17 @@ def caculate_zonal_statistics(i,out_dir,out_suffix,rast_fname,shp_fname,SRS_EPSG
     idx_pixelgeom = "idx_pixelgeom_%s" % (str(i))
     SQL_str = "CREATE INDEX %s ON %s USING GIST(pixelgeom);" % (idx_pixelgeom,poly_table)
     cur.execute(SQL_str)
-
-    SQL_str ="WITH temp_table AS (SELECT ST_Area((ST_Intersection(pixelgeom,geom)).geometry) AS intersectarea, pixelval, pixelarea AS origarea, cntycode FROM %s, %s WHERE ST_Intersects(pixelgeom,geom)) SELECT cntycode, SUM(pixelval*intersectarea/origarea) AS sums, SUM(pixelval*intersectarea)/SUM(intersectarea) AS means, COUNT(*) AS counts, MAX(pixelval) AS maxes, MIN(pixelval) AS mins, SUM(intersectarea)/1000000 AS area from temp_table group by cntycode;" % (poly_table,output_table_shp)
+    #Change cntycode to Name...also save the type column?--> make it a table...
+    #region_id_col
+    #SQL_str ="WITH temp_table AS (SELECT ST_Area((ST_Intersection(pixelgeom,geom)).geometry) AS intersectarea, pixelval, pixelarea AS origarea, cntycode FROM %s, %s WHERE ST_Intersects(pixelgeom,geom)) SELECT cntycode, SUM(pixelval*intersectarea/origarea) AS sums, SUM(pixelval*intersectarea)/SUM(intersectarea) AS means, COUNT(*) AS counts, MAX(pixelval) AS maxes, MIN(pixelval) AS mins, SUM(intersectarea)/1000000 AS area from temp_table group by cntycode;" % (poly_table,output_table_shp)
+    #SQL_str ="CREATE TABLE zonal_stat_regions AS WITH temp_table AS (SELECT ST_Area((ST_Intersection(pixelgeom,geom)).geometry) AS intersectarea, pixelval, pixelarea AS origarea, cntycode FROM %s, %s WHERE ST_Intersects(pixelgeom,geom)) SELECT cntycode, SUM(pixelval*intersectarea/origarea) AS sums, SUM(pixelval*intersectarea)/SUM(intersectarea) AS means, COUNT(*) AS counts, MAX(pixelval) AS maxes, MIN(pixelval) AS mins, SUM(intersectarea)/1000000 AS area from temp_table group by cntycode;" % (poly_table,output_table_shp)
+    SQL_str ="CREATE TABLE zonal_stat_regions AS WITH temp_table AS (SELECT ST_Area((ST_Intersection(pixelgeom,geom)).geometry) AS intersectarea, pixelval, pixelarea AS origarea, %s FROM %s, %s WHERE ST_Intersects(pixelgeom,geom)) SELECT %s, SUM(pixelval*intersectarea/origarea) AS sums, SUM(pixelval*intersectarea)/SUM(intersectarea) AS means, COUNT(*) AS counts, MAX(pixelval) AS maxes, MIN(pixelval) AS mins, SUM(intersectarea)/1000000 AS area from temp_table group by %s;" % (region_id_col,poly_table,output_table_shp,region_id_col,region_id_col)
 
     #cmd_str ="WITH temp_table AS (SELECT ST_Area((ST_Intersection(pixelgeom,geom)).geometry) AS intersectarea, pixelval, pixelarea AS origarea, cntycode FROM mht_poly_use, counties WHERE ST_Intersects(pixelgeom,geom)) SELECT cntycode, SUM(pixelval*intersectarea/origarea) AS sums, SUM(pixelval*intersectarea)/SUM(intersectarea) AS means, COUNT(*) AS counts, MAX(pixelval) AS maxes, MIN(pixelval) AS mins, SUM(intersectarea)/1000000 AS area from temp_table group by cntycode;"
-    cur.execute(SQL_str)          
+    cur.execute(SQL_str)       
+    SQL_str = "SELECT * FROM zonal_stat_regions"
+    cur.execute(SQL_str)       
+
     rows_table = cur.fetchall()
     
     #list_rows.append(rows)
@@ -329,6 +486,11 @@ def main():
     in_dir ="/ssi-dss-data/DSS_SSI_data"
     
     shp_fname = os.path.join(in_dir,"county24.shp")
+    region_name = "COUNTY" #name of the column containing the id for each entities...
+    #if region_name is None then create an ID? Add this option later on.
+    region_type = "C"    #type for the region entity: C for county, T for town  
+    #valueType = temperature
+
     #polygon_input_shp_file = "metwp24.shp"
 
     #EPSG: http://spatialreference.org/ref/epsg/26919/proj4/ -->  
@@ -367,14 +529,38 @@ def main():
 
     ########## START SCRIPT #############
          
-    ## Temp processing: 2020s,2030s,2040s,2050s for tmin, tmax and tmean (e.g. 4*3 directories with 12 files...)
+    
+    ### FIRST ADD A COLUMN NAME, VALUE TYPE
+    
+    #get counties names from shapefile,type=c,Time=year,valueType=tmin_1,tmax_1 etc., value
+    #test = shp_to_data_frame(shp_fname)
+    ### NOW REORGANIZE THE DATA INTO...
+    #Name,type,Time,valueType,Value    
+    #../county24.shp
+    
+    layer_name = os.path.splitext(os.path.basename(shp_fname))[0]
+    #os.system("ogrinfo "+shp_fname+" -sql \"SELECT COUNT(*) FROM "+layer_name+"\"")
+    nf = get_vct_FieldName(shp_fname)[1] #extract number of feature using user defined function (see above)
+    region_type_list = list(region_type)*nf
+    dict_region_type = {"Type":region_type_list}
+    
+    df_region = shp_to_data_frame(shp_fname)
+    region_name_list = list(df_region[region_name])
+    dict_region_name = {"Name":region_name_list}
+    #add_field_to_shp(in_fname,out_fname,dict_col=None):
+    out_fname1 = layer_name+"_out_"+out_suffix+".shp"
+    add_field_to_shp(shp_fname,out_fname1,dict_region_name)
+    #df_region1 = shp_to_data_frame(out_fname1)
+    out_fname2 = layer_name+"_out_"+out_suffix+".shp"
+    add_field_to_shp(out_fname1,out_fname2,dict_region_type)
+    df_region2 = shp_to_data_frame(out_fname1)
+                 
     ## Temp processing: 2020s,2030s,2040s,2050s for tmin, tmax and tmean (e.g. 4*3 directories with 12 files...)
 
     #1.download...not automated...
     #2.unzip asc or grd (arc grid)
     #3.convert to tif (gdal_tranlslate)
     #4.reproject and clip/subset for Maine region (clip using count24?)
-
 
     #Get tmin files
     l_f_tmin = filter(lambda x: re.search(r'tmin',x),l_f_temp)
@@ -383,9 +569,6 @@ def main():
     #Get tmean files
     l_f_tmean = filter(lambda x: re.search(r'tmean',x),l_f_temp)
     
-    list_rows_tmin = [] # defined lenth right now
-    
-    rast_fname = l_f_tmin #copy by refernce!!
     dict_rast_fname = {"tmin": l_f_tmin, "tmax": l_f_tmax, "tmean": l_f_tmean }
     #debug
     #test = pdb.runcall(caculate_zonal_statistics,i,out_dir,out_suffix,rast_fname,shp_fname,SRS_EPSG,postgres_path_bin,db_name,user_name)
@@ -395,6 +578,10 @@ def main():
     
     #This will be parallelized and looped through dict_rast_fname
     #can make one additional loop to reduce the repitition with tmin, tmax and tmean
+
+    list_rows_tmin = [] # defined lenth right now    
+    rast_fname = l_f_tmin #copy by refernce!!
+
     for i in range(0,len(rast_fname)):
     #for i in range(0,2):    
         out_suffix_s = "tmin_"+out_suffix
@@ -418,6 +605,8 @@ def main():
         out_suffix_s = "tmean_"+out_suffix
         rows = caculate_zonal_statistics(i,out_dir,out_suffix_s,rast_fname,shp_fname,SRS_EPSG,postgres_path_bin,db_name,user_name)
         list_rows_tmean.append(rows) #add object rows to list
+        #Note the first columns from row contains: region ID
+        #Other columns contain in hte order: region ID, sums, means, counts, maxes, mins, area
 
     #Write out results by combining
     #test1 =load_data(fname)
@@ -436,6 +625,7 @@ def main():
     fname = os.path.join(out_dir,fname)
     save_data(list_rows_tmax,fname)
     
+    list_rows_tmin = load_data("list_rows_min_05032014.dat")
     #list_rows_summary = [list_rows_tmin,list_rows_tmax,list_rows_tmean]
     dict_rows_summary = {"tmin":list_rows_tmin,"tmax":list_rows_tmax,"tmean":list_rows_tmean}
     dict_table = {}
@@ -476,26 +666,6 @@ def main():
         df_val["valueType"] = (list(df_var["var"]+"_"+df_var["month"]))*nb_rows
         df_val["type"]= (list("c"))*df.value.count() #using the number of count in column value
         del table
-
-
-    #### NOW REORGANIZE THE DATA INTO...
-    #Name,type,Time,valueType,Value    
-    #../county24.shp
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    ds =driver.Open(shp_fname,0)
-    lyr= ds.GetLayer(0)    
-    feature= lyr.GetFeature(0)
-
-    for feature in lyr:
-        print feature.GetField("COUNTY")
-        
-        
-    #get counties names from shapefile,type=c,Time=year,valueType=tmin_1,tmax_1 etc., value
-       
-    #table.ravel(order="F") #flatten the array by moving down columns
-    #extract_names from raster files...
-    #format_table_for_postgis
-    #create_summary_table(list_rows_min,nb_rows,nb_columns,out_dir,out_suffix)
         
     #Write function to add back the tables in postgis and join them?   
 
